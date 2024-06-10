bba2257dacff26f2c44c820b1ae16f8f40ef5cce
Nicky Hendrik Sen <nicky.hendriksen@yahoo.com>
2024-06-10 15:22:23 +0700

    Installed firebase emulator

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -12,4 +15,12 @@ const firebaseConfig = {
 
 const firebase = initializeApp(firebaseConfig);
 
+const auth = getAuth(firebase);
+const firestore = getFirestore(firebase);
+const functions = getFunctions(firebase);
+
+if (process.env.NODE_ENV === 'development') {
+  connectFirestoreEmulator(firestore, 'localhost', 8080);
+  connectFunctionsEmulator(functions, 'localhost', 5001);
+}
 export default firebase;
